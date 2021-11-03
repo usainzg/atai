7bcfe4db45b1949d596bd51e003672800c8e89c1
usainzg <unaihtc70@gmail.com>
2021-11-03 14:44:48 +0100

    Add task 1 linux

--- jgomas/bin/mas/jgomas_manager_task_1.sh
+++ jgomas/bin/mas/jgomas_manager_task_1.sh
@@ -1,3 +1,1 @@
-#!/bin/sh
-
-java -classpath "lib/jade.jar:lib/jadeTools.jar:lib/Base64.jar:lib/http.jar:lib/iiop.jar:lib/beangenerator.jar:lib/jgomas.jar:lib/jason.jar:lib/JasonJGomas.jar:classes:." jade.Boot -gui -host 127.0.0.1 "Manager:es.upv.dsic.gti_ia.jgomas.CManager(2,map_04,125,10)"
+java -classpath "lib/jade.jar:lib/jadeTools.jar:lib/Base64.jar:lib/http.jar:lib/iiop.jar:lib/beangenerator.jar:lib/jgomas.jar:lib/jason.jar:lib/JasonJGomas.jar:classes:." jade.Boot -gui -host 127.0.0.1 "Manager:es.upv.dsic.gti_ia.jgomas.CManager(1,map_04,125,10)"
